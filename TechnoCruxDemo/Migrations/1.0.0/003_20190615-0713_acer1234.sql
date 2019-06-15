fe200017b4d01cb156608f2c6a96882d69e9d8c8
davidcq23 <davidcq23@gmail.com>
2019-06-15 07:14:05 -0600

    Se agrega un store procedure

---
+++ TechnoCruxDemo/Migrations/1.0.0/003_20190615-0713_acer1234.sql
@@ -0,0 +1,20 @@
+﻿-- <Migration ID="674e5e60-d9ff-4bf8-884c-184455735edf" />
+GO
+
+PRINT N'Creating [dbo].[ConsultaCliente]'
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[ConsultaCliente]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	Select * from Cliente
+END
+GO
