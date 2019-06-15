69ca8057e77cd46fac39612d275755fe3659dfcb
davidcq23 <davidcq23@gmail.com>
2019-06-15 11:48:28 -0600

    Agregando tabla factura

---
+++ TechnoCruxDemo/Migrations/1.0.0/004_20190615-1147_acer1234.sql
@@ -0,0 +1,12 @@
+﻿-- <Migration ID="fc6e01a7-27dc-4f12-9420-5fc9fa0a16f4" />
+GO
+
+PRINT N'Creating [dbo].[Factura]'
+GO
+CREATE TABLE [dbo].[Factura]
+(
+[IDFactura] [nchar] (10) NULL,
+[Nombre] [nchar] (10) NULL,
+[Monto] [int] NULL
+)
+GO
---
+++ Test2/Migrations/0001_20190615-1143_acer1234.sql
@@ -0,0 +1,31 @@
+﻿-- <Migration ID="47a25df6-2f96-4a7b-b4a9-0e340c1b6d8c" />
+GO
+
+PRINT N'Creating [dbo].[Cliente]'
+GO
+CREATE TABLE [dbo].[Cliente]
+(
+[IDCliente] [int] NOT NULL,
+[Nombre] [nchar] (10) NULL,
+[Apellido] [nchar] (10) NULL,
+[Telefono] [nchar] (10) NULL,
+[Direccion] [nchar] (10) NULL
+)
+GO
+PRINT N'Creating [dbo].[ConsultaCliente]'
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[ConsultaCliente]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	Select * from Cliente
+END
+GO
