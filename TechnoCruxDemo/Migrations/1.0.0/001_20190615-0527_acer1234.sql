3baf481b8d420fbd5dd4e4a58043b38055881bc4
davidcq23 <davidcq23@gmail.com>
2019-06-15 05:29:41 -0600

    Se agrega base de datos

---
+++ TechnoCruxDemo/Migrations/1.0.0/001_20190615-0527_acer1234.sql
@@ -0,0 +1,13 @@
+﻿-- <Migration ID="60381018-db88-484f-8fbf-c8a570499961" />
+GO
+
+PRINT N'Creating [dbo].[Cliente]'
+GO
+CREATE TABLE [dbo].[Cliente]
+(
+[IDCliente] [int] NOT NULL,
+[Nombre] [nchar] (10) NULL,
+[Apellido] [nchar] (10) NULL,
+[Telefono] [nchar] (10) NULL
+)
+GO
