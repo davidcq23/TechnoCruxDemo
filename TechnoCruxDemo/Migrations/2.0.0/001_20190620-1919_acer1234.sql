1e7cc24344f0c3ba4e1b1751cef45e82d458f94e
davidcq23 <davidcq23@gmail.com>
2019-06-20 19:20:39 -0600

    Agregar nueva tabla

---
+++ TechnoCruxDemo/Migrations/2.0.0/001_20190620-1919_acer1234.sql
@@ -0,0 +1,14 @@
+﻿-- <Migration ID="9472c1c2-c829-4595-b549-4695f54a8476" />
+GO
+
+PRINT N'Creating [dbo].[Table]'
+GO
+CREATE TABLE [dbo].[Table]
+(
+[Id] [int] NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK__Table__3214EC07A61736C4] on [dbo].[Table]'
+GO
+ALTER TABLE [dbo].[Table] ADD CONSTRAINT [PK__Table__3214EC07A61736C4] PRIMARY KEY CLUSTERED  ([Id])
+GO
